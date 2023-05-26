bb5b9fab809cf74161116ecca751d00c772d0c61
unknown <sairam.pulagam@gmail.com>
2023-05-26 10:06:14 +0530

    This code is WOrkspace and Remote Sate FIle)

---
+++ main.tf
@@ -0,0 +1,15 @@
+#This Terraform Code Deploys Basic VPC Infra.
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+
+#Terraform Backend S3
+terraform {
+  backend "s3" {
+    bucket = "gvfxflowlogbuckets3"
+    key    = "gvfxflow.tfstate"
+    region = "us-east-1"
+  }
+}
