c519c00beb12b5b79b9cc90d83bed917ec8e6676
friends4433 <76154446+friends4433@users.noreply.github.com>
2023-06-16 23:52:21 +0530

    Update S3.tf

--- S3.tf
+++ S3.tf
@@ -1,6 +1,6 @@
-resource "aws_s3_bucket" "gvfxflowlogbuckets3"{
-  bucket = "gvfxflowlogbuckets3"
-  tags = {
-    Name = "Bucket_Tag"
-  }
-}
+#resource "aws_s3_bucket" "gvfxflowlogbuckets3"{
+#  bucket = "gvfxflowlogbuckets3"
+#  tags = {
+#    Name = "Bucket_Tag"
+#  }
+#}
